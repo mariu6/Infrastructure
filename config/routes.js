70825dd3c7a6233004de0d7a8ff4edc765211c36
Mariusz Ilijew <mariu6@gmail.com>
2020-06-26 16:54:19 +0200

    feat: package.json and more

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,10 @@
-const routes = require("../routes");
+const routes = require("../routes");        // exported from index.js 
 
 module.exports = (app) => {
-    // TO DO - All the main routes the application will have
+    // TO DO - All the main routes the application I have
+
+    app.use("/home", routes.home);            // parent routes      
+    app.use("/users", routes.users);
+
+    app.use("/EVENTS", routes.EVENTS);
 }
